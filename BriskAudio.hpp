14b51f0117a339b524a5d63e6ccd3eb42e046adc
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-06-17 18:16:23 +0530

    Update BriskAudio.hpp

--- BriskAudio.hpp
+++ BriskAudio.hpp
@@ -1,10 +1,12 @@
 #include <vector>
 #include <string>
 
-#include "BriskAudioLinux.h"
-#include "BriskAudioWindows.h"
-
 namespace BriskAudio {
+    enum ExitStatus {
+        SUCCESS,
+        FAILURE
+    };
+
     enum class BufferFormat {
         U_INT_8,
         S_INT_16,
@@ -20,11 +22,25 @@ namespace BriskAudio {
     struct DeviceInfo {
         std::string name;
         std::string ID;
+        bool isDefault;
         DeviceType deviceType;
+        unsigned int minInputChannels;
+        unsigned int minOutputChannels;
         unsigned int maxInputChannels;
         unsigned int maxOutputChannels;
-        std::vector<unsigned int> sampleRates;
+        std::vector<unsigned int> supportedSampleRates;
         std::vector<unsigned int> supportedBufferSizes;
         BufferFormat supportedBufferFormats;
+
+        DeviceInfo() {
+            isDefault = false;
+            minInputChannels = 0;
+            minOutputChannels = 0;
+            maxInputChannels = 0;
+            maxOutputChannels = 0;
+        }
+
+        unsigned int getDeviceCount();
+        DeviceInfo getDeviceInfo(unsigned int i);
     };
 }
