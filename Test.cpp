a43e9012f0611dd83d7f554f8d96fed5d8a7352f
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-06-19 10:23:37 +0530

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -3,7 +3,16 @@
 #include "BriskAudio.hpp"
 
 int main() {
-    BriskAudio::DeviceInfo info;
+    BriskAudio::DeviceEnumerator deviceEnumerator;
+    BriskAudio::DeviceInfo deviceInfo;
 
-    std::cout << "Device count: " << info.getDeviceCount() << '\n';
+    for (int i = 0; i < deviceEnumerator.getDeviceCount(); i++) {
+        deviceInfo = deviceEnumerator.getDeviceInfo(i);
+
+        if (deviceInfo.isValid) {
+            std::cout << "Name: " << deviceInfo.name << '\n';
+            std::cout << "Description: " << deviceInfo.description << '\n';
+            unsigned int i  = deviceInfo.supportedSampleRates[0];
+        }
+    }
 }
