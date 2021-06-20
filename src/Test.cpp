3c2832bc2d6d02cf42840ae389ec5adb5ce69bc7
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-06-20 16:04:17 +0530

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -5,34 +5,34 @@
 using namespace BriskAudio;
 
 int main() {
-    DeviceEnumerator deviceEnumerator;
-    DeviceInfo deviceInfo;
+    DeviceInfoCollection collection;
+    DeviceInfo info;
 
-    unsigned int playbackDeviceCount = deviceEnumerator.getDeviceCount(DeviceType::PLAYBACK);
+    unsigned int playbackDeviceCount = collection.getDeviceCount(DeviceType::PLAYBACK);
 
     std::cout << "\nPlayback device count: " << playbackDeviceCount << "\n\n";
 
     // Display playback device details if successfully probed
     for (unsigned int i = 0; i < playbackDeviceCount; i++) {
-        deviceInfo = deviceEnumerator.getDeviceInfo(i, DeviceType::PLAYBACK);
+        info = collection.getDeviceInfo(i, DeviceType::PLAYBACK);
 
-        if (deviceInfo.isValid) {
-            std::cout << "Name: " << deviceInfo.name << '\n';
-            std::cout << "Description: " << deviceInfo.description << "\n\n";
+        if (info.isValid) {
+            std::cout << "Name: " << info.name << '\n';
+            std::cout << "Description: " << info.description << "\n\n";
         }
     }
 
-    unsigned int captureDeviceCount = deviceEnumerator.getDeviceCount(DeviceType::CAPTURE);
+    unsigned int captureDeviceCount = collection.getDeviceCount(DeviceType::CAPTURE);
 
     std::cout << "Capture device count: " << captureDeviceCount << "\n\n";
 
     // Display capture device details if successfully probed
     for (unsigned int i = 0; i < captureDeviceCount; i++) {
-        deviceInfo = deviceEnumerator.getDeviceInfo(i, DeviceType::CAPTURE);
+        info = collection.getDeviceInfo(i, DeviceType::CAPTURE);
 
-        if (deviceInfo.isValid) {
-            std::cout << "Name: " << deviceInfo.name << '\n';
-            std::cout << "Description: " << deviceInfo.description << "\n\n";
+        if (info.isValid) {
+            std::cout << "Name: " << info.name << '\n';
+            std::cout << "Description: " << info.description << "\n\n";
         }
     }
 
