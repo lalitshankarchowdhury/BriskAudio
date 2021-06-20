856d8877cfafb89554ae4127859451a110960055
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-06-20 15:18:41 +0530

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -10,39 +10,31 @@ int main() {
 
     unsigned int playbackDeviceCount = deviceEnumerator.getDeviceCount(DeviceType::PLAYBACK);
 
-    std::cout << "--------------------------------------------------------------------------------" << '\n';
+    std::cout << "\nPlayback device count: " << playbackDeviceCount << "\n\n";
 
-    std::cout << "Playback device count: " << playbackDeviceCount << '\n';
-
-    // Display device details if successfully probed
+    // Display playback device details if successfully probed
     for (unsigned int i = 0; i < playbackDeviceCount; i++) {
         deviceInfo = deviceEnumerator.getDeviceInfo(i, DeviceType::PLAYBACK);
 
         if (deviceInfo.isValid) {
-            std::cout << "--------------------------------------------------------------------------------" << '\n';
             std::cout << "Name: " << deviceInfo.name << '\n';
-            std::cout << "Description: " << deviceInfo.description << '\n';
+            std::cout << "Description: " << deviceInfo.description << "\n\n";
         }
     }
 
     unsigned int captureDeviceCount = deviceEnumerator.getDeviceCount(DeviceType::CAPTURE);
 
-    std::cout << "--------------------------------------------------------------------------------" << '\n';
+    std::cout << "Capture device count: " << captureDeviceCount << "\n\n";
 
-    std::cout << "Capture device count: " << captureDeviceCount << '\n';
-
-    // Display device details if successfully probed
+    // Display capture device details if successfully probed
     for (unsigned int i = 0; i < captureDeviceCount; i++) {
         deviceInfo = deviceEnumerator.getDeviceInfo(i, DeviceType::CAPTURE);
 
         if (deviceInfo.isValid) {
-            std::cout << "--------------------------------------------------------------------------------" << '\n';
             std::cout << "Name: " << deviceInfo.name << '\n';
-            std::cout << "Description: " << deviceInfo.description << '\n';
+            std::cout << "Description: " << deviceInfo.description << "\n\n";
         }
     }
-
-    std::cout << "--------------------------------------------------------------------------------" << '\n';
 
     std::getchar();
 }
