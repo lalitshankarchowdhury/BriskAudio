01362bf7a801b497f514813d7f23f3aa567b58ae
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-06-22 09:46:38 +0530

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
 #include "BriskAudio.hpp"
+
+#define GREEN "\u001b[32m"
+#define RESET "\u001b[0m"
 
 using namespace BriskAudio;
 
@@ -14,31 +16,34 @@ int main() {
 
     unsigned int playbackDeviceCount = collection.getDeviceCount(DeviceType::PLAYBACK);
 
+    std::cout << "--------------------------------------------------------------------------------\n";
     std::cout << "\nPlayback device count: " << playbackDeviceCount << "\n\n";
+    std::cout << "--------------------------------------------------------------------------------\n";
 
     // Display playback device details if successfully probed
     for (unsigned int i = 0; i < playbackDeviceCount; i++) {
         info = collection.getDeviceInfo(i, DeviceType::PLAYBACK);
 
         if (info.isValid) {
-            std::cout << "Name: " << info.name << '\n';
-            std::cout << "ID: " << info.ID << '\n';
-            std::cout << "Description: " << info.description << "\n\n";
+            std::cout << GREEN << info.name << RESET << '\n';
+            std::cout << info.description << '\n';
+            std::cout << "--------------------------------------------------------------------------------\n";
         }
     }
 
     unsigned int captureDeviceCount = collection.getDeviceCount(DeviceType::CAPTURE);
 
-    std::cout << "Capture device count: " << captureDeviceCount << "\n\n";
+    std::cout << "\nCapture device count: " << captureDeviceCount << "\n\n";
+    std::cout << "--------------------------------------------------------------------------------\n";
 
     // Display capture device details if successfully probed
     for (unsigned int i = 0; i < captureDeviceCount; i++) {
         info = collection.getDeviceInfo(i, DeviceType::CAPTURE);
 
         if (info.isValid) {
-            std::cout << "Name: " << info.name << '\n';
-            std::cout << "ID: " << info.ID << '\n';
-            std::cout << "Description: " << info.description << "\n\n";
+            std::cout << GREEN << info.name << RESET << '\n';
+            std::cout << info.description << '\n';
+            std::cout << "--------------------------------------------------------------------------------\n";
         }
     }
 
