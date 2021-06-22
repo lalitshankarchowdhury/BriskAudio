336577d545ee8ce6fbfb91473909e87b9627276b
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-06-22 09:44:50 +0530

    Update BriskAudioLinux.cpp

--- src/BriskAudioLinux.cpp
+++ src/BriskAudioLinux.cpp
@@ -1,26 +1,93 @@
+#ifdef __linux
 #include "BriskAudio.hpp"
 #include <alsa/asoundlib.h>
+#include <iostream>
 
-static char** hints;
+static void** hints;
 
 namespace BriskAudio {
-    unsigned int DeviceInfoCollection::getDeviceCount(DeviceType aType) {
-        if (snd_device_name_hint(-1, "pcm", (void***) &hints) != 0) {
-            return 0;
+    Exit init() {
+        if (snd_device_name_hint(-1, "pcm", &hints) < 0) {
+            return Exit::FAILURE;
         }
 
-        unsigned int count = 0;
+        return Exit::SUCCESS;
+    }
 
-        for (char** n = hints; *n != NULL; n++) {
-            count++;
+    unsigned int DeviceInfoCollection::getDeviceCount(DeviceType aType) {
+        unsigned int count = 0;
+        char* IOID;
+
+        const char* filter = (aType == DeviceType::PLAYBACK)? "Output" : "Input";
+        
+        for (void** n = hints; *n != NULL; n++) {
+            IOID = snd_device_name_get_hint(*n, "IOID");
+
+            if (IOID != nullptr) {
+                if (strcmp(IOID, filter) == 0) {
+                    count++;
+                }
+
+                free(IOID);
+            } else {
+                // If IOID cannot be determined
+                count++;
+            }
         }
 
         return count;
     }
 
     DeviceInfo DeviceInfoCollection::getDeviceInfo(unsigned int aIndex, DeviceType aType) {
+        unsigned int count = 0;
+        char* NAME;
+        char* DESC;
+        char* IOID;
         DeviceInfo temp;
+
+        const char* filter = (aType == DeviceType::PLAYBACK)? "Output" : "Input";
+        
+        for (void** n = hints; *n != NULL; n++) {
+            IOID = snd_device_name_get_hint(*n, "IOID");
+
+            if (IOID != nullptr) {
+                if (strcmp(IOID, filter) == 0) {
+                    count++;
+                }
+
+                free(IOID);
+            } else {
+                // If IOID cannot be determined
+                count++;
+            }
+
+            // If PCM device is found
+            if ((count- 1) == aIndex) {
+                NAME = snd_device_name_get_hint(*n, "NAME");
+                DESC = snd_device_name_get_hint(*n, "DESC");
+
+                temp.name = NAME;
+                temp.description = DESC;
+
+                temp.isValid = true;
+
+                if (NAME != nullptr) {
+                    free(NAME);
+                }
+
+                if (DESC != nullptr) {
+                    free(DESC);
+                }
+
+                break;
+            }
+        }
 
         return temp;
     }
+
+    void quit() {
+        snd_device_name_free_hint(hints);
+    }
 }
+#endif
