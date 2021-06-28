ab0fd0342cad0dd5beb05ae9af9baa8e8a5c8a30
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-06-28 15:54:59 +0530

    Update BriskAudio.hpp

--- include/BriskAudio.hpp
+++ include/BriskAudio.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace BriskAudio {
 enum class Exit {
@@ -11,6 +12,46 @@ enum class Exit {
 enum class EndpointType {
     PLAYBACK,
     CAPTURE
+};
+
+enum class BufferFormat {
+    U_INT_8,
+    S_INT_16,
+    S_INT_24,
+    S_INT_32,
+    FLOAT_32,
+    FLOAT_64
+};
+
+struct StreamConfig {
+    bool isValid;
+    unsigned int numChannels;
+    unsigned int sampleRate;
+    BufferFormat format;
+
+    StreamConfig()
+    {
+        isValid = false;
+        numChannels = 0;
+        sampleRate = 0;
+        formats = (BufferFormat) 0;
+    }
+};
+
+struct Stream {
+    void* nativeHandle;
+
+    Stream() {
+        nativeHandle = nullptr;
+    }
+
+    unsigned int getBufferSize();
+    Exit setBufferSize(unsigned int aSize);
+    Exit start();
+    Exit stop();
+
+    private:
+        unsigned int bufferSize_;
 };
 
 struct Endpoint {
@@ -24,9 +65,15 @@ struct Endpoint {
     {
         isValid = false;
         nativeHandle = nullptr;
+        cardName = "INVALID";
+        description = "INVALID";
+        type = (EndpointType) 0;
     }
 
     void releaseNativeHandle();
+    StreamConfig getSupportedStreamConfigs();
+    Exit openStream(Stream* aStream);
+    Exit closeStream(Stream* aStream);
 };
 
 struct EndpointEnumerator {
