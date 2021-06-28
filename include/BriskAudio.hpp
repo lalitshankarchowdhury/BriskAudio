1a4f8e974299dd5478e6ab7b17d34041dae0dcba
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-06-28 13:43:42 +0530

    Create BriskAudio.hpp

---
+++ include/BriskAudio.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+
+namespace BriskAudio {
+enum class Exit {
+    SUCCESS,
+    FAILURE
+};
+
+enum class EndpointType {
+    PLAYBACK,
+    CAPTURE
+};
+
+struct Endpoint {
+    bool isValid;
+    void* nativeHandle;
+    std::string cardName;
+    std::string description;
+    EndpointType type;
+
+    Endpoint()
+    {
+        isValid = false;
+        nativeHandle = nullptr;
+    }
+
+    void releaseNativeHandle();
+};
+
+struct EndpointEnumerator {
+    EndpointType type;
+
+    unsigned int getEndpointCount();
+    Endpoint getDefaultEndpoint();
+    Endpoint getEndpoint(unsigned int aIndex);
+};
+
+Exit init();
+Exit quit();
+}
