4cd1adb5fbc8c0cf37d07a9248b3974a6fac723f
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-07-08 20:10:15 +0530

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -2,32 +2,27 @@
 #include <assert.h>
 #include <iostream>
 
-#define RED "\u001b[31m"
-#define GREEN "\u001b[32m"
-#define BLUE "\u001b[34m"
-#define RESET "\u001b[0m"
-
 using namespace BriskAudio;
 
 void onDefaultDeviceChange(std::string aDeviceName, DeviceType aType)
 {
     const char* type = (aType == DeviceType::PLAYBACK) ? "output" : "input";
 
-    std::cout << BLUE << "Default " << type << " device changed to: " << aDeviceName + '\n' + RESET;
+    std::cout << "Default " << type << " device changed to: " << aDeviceName + '\n';
 }
 
 void onDeviceAdd(std::string aDeviceName, DeviceType aType)
 {
     const char* type = (aType == DeviceType::PLAYBACK) ? "Output" : "Input";
 
-    std::cout << GREEN << type << " device added: " << aDeviceName + '\n' + RESET;
+    std::cout << type << " device added: " << aDeviceName + '\n';
 }
 
 void onDeviceRemove(std::string aDeviceName, DeviceType aType)
 {
     const char* type = (aType == DeviceType::PLAYBACK) ? "Output" : "Input";
 
-    std::cout << RED << type << " device removed: " << aDeviceName + '\n' + RESET;
+    std::cout << type << " device removed: " << aDeviceName + '\n';
 }
 
 int main()
