619f723bdd392b241c5df87a856bde393c2779c1
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-07-10 21:44:20 +0530

    Update BriskAudio.hpp

--- include/BriskAudio.hpp
+++ include/BriskAudio.hpp
@@ -16,16 +16,33 @@ enum class DeviceType {
 struct Device {
     std::string name;
     DeviceType type;
-    void* nativeHandle;
 
     Device()
     {
         name = "INVALID";
         type = DeviceType::PLAYBACK;
-        nativeHandle = nullptr;
+        nativeHandle_ = nullptr;
     }
 
     ~Device();
+
+private:
+    void* nativeHandle_;
+
+    friend struct DeviceEnumerator;
+};
+
+struct DeviceEnumerator {
+    DeviceType type;
+
+    DeviceEnumerator(DeviceType aType)
+    {
+        type = aType;
+    }
+
+    unsigned int deviceCount();
+    Device* giveDefaultDevice();
+    Device* giveDevice(unsigned int aIndex);
 };
 
 extern void (*pOnDefaultDeviceChange)(std::string aDeviceName, DeviceType aType);
@@ -33,8 +50,5 @@ extern void (*pOnDeviceAdd)(std::string aDeviceName, DeviceType aType);
 extern void (*pOnDeviceRemove)(std::string aDeviceName, DeviceType aType);
 
 Exit init();
-Exit getDeviceCount(DeviceType aType, unsigned int& aCount);
-Exit getDefaultDevice(DeviceType aType, Device& appDevice);
-Exit getDevice(unsigned int aIndex, DeviceType aType, Device& appDevice);
 Exit quit();
 }
