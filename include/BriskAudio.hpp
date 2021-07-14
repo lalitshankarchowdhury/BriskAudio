6b153917357a560dc71bf63f449a54b81f7cad5d
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-07-15 01:13:37 +0530

    Update BriskAudio.hpp

--- include/BriskAudio.hpp
+++ include/BriskAudio.hpp
@@ -22,43 +22,39 @@ enum class BufferFormat {
     FLOAT_64
 };
 
+struct Stream {
+private:
+    BufferFormat format_;
+    unsigned int numChannels_;
+    unsigned int sampleRate_;
+};
+
 struct Device {
     std::string name;
     DeviceType type;
+    void* nativeHandle;
 
     Device()
     {
         name = "??????";
         type = DeviceType::PLAYBACK;
+        nativeHandle = nullptr;
     }
 
     ~Device();
 
     bool isStreamFormatSupported(BufferFormat aFormat, unsigned int aNumChannels, unsigned int aSampleRate);
-
-private:
-    void* nativeHandle_;
-
-    friend struct DeviceEnumerator;
-};
-
-struct DeviceEnumerator {
-    DeviceType type;
-
-    DeviceEnumerator()
-    {
-        type = DeviceType::PLAYBACK;
-    }
-
-    ~DeviceEnumerator() {}
-
-    unsigned int returnDeviceCount();
-    Device* returnDefaultDevice();
-    Device* returnDevice(unsigned int aIndex);
+    Exit openStream(Stream& arStream, BufferFormat aFormat, unsigned int aNumChannels, unsigned int aSampleRate);
+    Exit closeStream(Stream& arStream);
 };
 
 struct DeviceEventNotifier;
 
 Exit init();
+Exit getDeviceCount(unsigned int& arDeviceCount, DeviceType aType);
+Exit openDefaultDevice(Device& arDevice, DeviceType aType);
+Exit openDevice(Device& arDevice, unsigned int aIndex, DeviceType aType);
+Exit openDevice(Device& arDevice, std::string aDeviceName);
+Exit closeDevice(Device& arDevice);
 Exit quit();
 }
