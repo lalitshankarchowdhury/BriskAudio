64634b64f711add3f3e4dc420134537678ec1d2e
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-07-15 07:39:05 +0530

    Update BriskAudioWindows.hpp

--- src/BriskAudioWindows.hpp
+++ src/BriskAudioWindows.hpp
@@ -1,33 +1,33 @@
 #pragma once
 
 #ifdef _WIN32
-#include "../include/BriskAudio.hpp"
+#include <Endpointvolume.h>
 #include <mmdeviceapi.h>
+#include <iostream>
 
 namespace BriskAudio {
-struct DeviceEventNotifier : IMMNotificationClient {
-    DeviceEventNotifier();
-    virtual ~DeviceEventNotifier();
-    ULONG STDMETHODCALLTYPE AddRef();
-    ULONG STDMETHODCALLTYPE Release();
-    HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, VOID** ppvInterface);
-    HRESULT STDMETHODCALLTYPE OnDefaultDeviceChanged(EDataFlow flow, ERole role, LPCWSTR pwstrDeviceId);
-    HRESULT STDMETHODCALLTYPE OnDeviceAdded(LPCWSTR pwstrDeviceId);
-    HRESULT STDMETHODCALLTYPE OnDeviceRemoved(LPCWSTR pwstrDeviceId);
-    HRESULT STDMETHODCALLTYPE OnDeviceStateChanged(LPCWSTR pwstrDeviceId, DWORD dwNewState);
-    HRESULT STDMETHODCALLTYPE OnPropertyValueChanged(LPCWSTR pwstrDeviceId, const PROPERTYKEY key);
-    Exit registerEventCallbacks(
-        void (*apOnDefaultDeviceChange)(std::string aDeviceName, DeviceType aType),
-        void (*apOnDeviceAdd)(std::string aDeviceName, DeviceType aType),
-        void (*apOnDeviceRemove)(std::string aDeviceName, DeviceType aType));
-    Exit unregisterEventCallbacks();
+struct NativeDeviceHandle {
+    IMMDevice* pDevice;
+    IAudioEndpointVolume* pVolume;
 
-private:
-    LONG cRef_;
-    LPCWSTR pDeviceId_;
-    void (*pOnDefaultDeviceChange_)(std::string aDeviceName, DeviceType aType);
-    void (*pOnDeviceAdd_)(std::string aDeviceName, DeviceType aType);
-    void (*pOnDeviceRemove_)(std::string aDeviceName, DeviceType aType);
+    NativeDeviceHandle()
+    {
+        pDevice = nullptr;
+        pVolume = nullptr;
+    }
+
+    ~NativeDeviceHandle()
+    {
+        if (pVolume != nullptr) {
+            pVolume->Release();
+            pVolume = nullptr;
+        }
+
+        if (pDevice != nullptr) {
+            pDevice->Release();
+            pDevice = nullptr;
+        }
+    }
 };
 }
 #endif
