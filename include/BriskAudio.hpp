7a086cacadd04b45e9116b403f7944302e3ad7ae
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-07-16 00:38:04 +0530

    Create BriskAudio.hpp

---
+++ include/BriskAudio.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "../src/BriskAudioWindows.hpp"
+#include <string>
+
+namespace BriskAudio {
+enum class Exit {
+    SUCCESS,
+    FAILURE
+};
+
+enum class DeviceType {
+    PLAYBACK,
+    CAPTURE
+};
+
+enum class BufferFormat {
+    U_INT_8,
+    S_INT_16,
+    S_INT_24,
+    S_INT_32,
+    FLOAT_32,
+    FLOAT_64
+};
+
+// NativeDeviceHandle is defined in platform-specific headers and contains pointers to device event callbacks
+struct Device : NativeDeviceHandle {
+    std::string name;
+    DeviceType type;
+
+    Device()
+    {
+        name = "??????";
+        type = DeviceType::PLAYBACK;
+    }
+
+    Exit getVolume(float& arVolume);
+    Exit setVolume(float aVolume);
+    bool isStreamFormatSupported(BufferFormat aFormat, unsigned int aNumChannels, unsigned int aSampleRate);
+};
+
+Exit init();
+Exit getDeviceCount(unsigned int& arDeviceCount, DeviceType aType);
+Exit openDefaultDevice(Device& arDevice, DeviceType aType);
+Exit openDevice(Device& arDevice, unsigned int aIndex, DeviceType aType);
+Exit openDevice(Device& arDevice, std::string aDeviceName);
+Exit closeDevice(Device& arDevice);
+Exit quit();
+}
