55c0f4ae90c4184338746f32e9c09619586bc6fa
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-07-18 04:55:55 +0530

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -33,33 +33,40 @@ int main()
 {
     assert(init() == Exit::SUCCESS);
 
-    Device device;
-
-    assert(openDefaultDevice(device, DeviceType::PLAYBACK) == Exit::SUCCESS);
-
-    std::cout << "Device name: " << device.name << '\n';
+	unsigned int count = 0;
 	
-    std::cout << "Supported channels:";
-    for (unsigned int numChannels : device.supportedChannels) {
-        std::cout << ' ' << numChannels;
-    }
-    std::cout << '\n';
+	assert(getDeviceCount(count, DeviceType::CAPTURE) == Exit::SUCCESS);
+	
+	for (unsigned int i = 0; i < count; i++) {
+		Device device;
+		assert(openDevice(device, i, DeviceType::CAPTURE) == Exit::SUCCESS);
 
-    std::cout << "Supported sample rates:";
-    for (unsigned int sampleRate : device.sampleRates) {
-        std::cout << ' ' << sampleRate;
-    }
-    std::cout << '\n';
+		std::cout << "Device name: " << device.name << '\n';
+		
+		std::cout << "Supported channels:";
+		for (unsigned int numChannels : device.supportedChannels) {
+			std::cout << ' ' << numChannels;
+		}
+		std::cout << '\n';
 
-    device.pOnVolumeChange = onVolumeChange;
-    device.pOnMute = onMute;
-    device.pOnDefaultDeviceChange = onDefaultDeviceChange;
-    device.pOnDeviceAdd = onDeviceAdd;
-    device.pOnDeviceRemove = onDeviceRemove;
+		std::cout << "Supported sample rates:";
+		for (unsigned int sampleRate : device.sampleRates) {
+			std::cout << ' ' << sampleRate;
+		}
+		std::cout << '\n';
+		
+		std::cout << "Buffer format mask: 0x" << std::hex << static_cast<unsigned int>(device.supportedFormats) << '\n';
 
-    getchar();
+		device.pOnVolumeChange = onVolumeChange;
+		device.pOnMute = onMute;
+		device.pOnDefaultDeviceChange = onDefaultDeviceChange;
+		device.pOnDeviceAdd = onDeviceAdd;
+		device.pOnDeviceRemove = onDeviceRemove;
 
-    assert(closeDevice(device) == Exit::SUCCESS);
+		getchar();
+
+		assert(closeDevice(device) == Exit::SUCCESS);
+	}
 
     assert(quit() == Exit::SUCCESS);
 }
