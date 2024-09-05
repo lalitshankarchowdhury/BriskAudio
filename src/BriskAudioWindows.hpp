5ed42ceb82f318a8db0faac522ed9577d35dc7ec
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2024-09-05 12:15:58 +0530

    Refactor and reformat BriskAudioWindows.hpp

--- src/BriskAudioWindows.hpp
+++ src/BriskAudioWindows.hpp
@@ -9,39 +9,39 @@
 #include <string>
 
 namespace BriskAudio {
-struct NativeStreamHandle {
-    CComPtr<IAudioRenderClient> pRenderClient;
-    CComPtr<IAudioCaptureClient> pCaptureClient;
+	struct NativeStreamHandle {
+		CComPtr<IAudioRenderClient> pRenderClient;
+		CComPtr<IAudioCaptureClient> pCaptureClient;
 
-    NativeStreamHandle();
-};
+		NativeStreamHandle();
+	};
 
-struct NativeDeviceHandle : public IAudioEndpointVolumeCallback, public IMMNotificationClient {
-    CComPtr<IMMDevice> pDevice;
-    CComPtr<IAudioClient> pClient;
-    CComPtr<IAudioEndpointVolume> pVolume;
-    GUID eventContext;
-    void (*pOnVolumeChange)(float aVolume);
-    void (*pOnMute)();
-    void (*pOnDefaultDeviceChange)(std::string aDeviceName);
-    void (*pOnDeviceAdd)(std::string aDeviceName);
-    void (*pOnDeviceRemove)(std::string aDeviceName);
+	struct NativeDeviceHandle : public IAudioEndpointVolumeCallback, public IMMNotificationClient {
+		CComPtr<IMMDevice> pDevice;
+		CComPtr<IAudioClient> pClient;
+		CComPtr<IAudioEndpointVolume> pVolume;
+		GUID eventContext;
+		void (*pOnVolumeChange)(std::string aDeviceName, float aVolume);
+		void (*pOnMute)(std::string aDeviceName);
+		void (*pOnDefaultDeviceChange)(std::string aDeviceName);
+		void (*pOnAnyDeviceAdd)(std::string aDeviceName);
+		void (*pOnAnyDeviceRemove)(std::string aDeviceName);
+		void (*pOnAnyDeviceStateChange)(std::string aDeviceName, std::string state);
 
-    NativeDeviceHandle();
-    virtual ~NativeDeviceHandle();
-    ULONG STDMETHODCALLTYPE AddRef();
-    ULONG STDMETHODCALLTYPE Release();
-    HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, VOID** ppvInterface);
-    HRESULT STDMETHODCALLTYPE OnDefaultDeviceChanged(EDataFlow flow, ERole role, LPCWSTR pwstrDeviceId);
-    HRESULT STDMETHODCALLTYPE OnDeviceAdded(LPCWSTR pwstrDeviceId);
-    HRESULT STDMETHODCALLTYPE OnDeviceRemoved(LPCWSTR pwstrDeviceId);
-    HRESULT STDMETHODCALLTYPE OnDeviceStateChanged(LPCWSTR pwstrDeviceId, DWORD dwNewState);
-    HRESULT STDMETHODCALLTYPE OnPropertyValueChanged(LPCWSTR pwstrDeviceId, const PROPERTYKEY key);
-    HRESULT STDMETHODCALLTYPE OnNotify(PAUDIO_VOLUME_NOTIFICATION_DATA pNotify);
+		NativeDeviceHandle();
+		virtual ~NativeDeviceHandle();
+		ULONG STDMETHODCALLTYPE AddRef();
+		ULONG STDMETHODCALLTYPE Release();
+		HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, VOID** ppvInterface);
+		HRESULT STDMETHODCALLTYPE OnDefaultDeviceChanged(EDataFlow flow, ERole role, LPCWSTR pwstrDeviceId);
+		HRESULT STDMETHODCALLTYPE OnDeviceAdded(LPCWSTR pwstrDeviceId);
+		HRESULT STDMETHODCALLTYPE OnDeviceRemoved(LPCWSTR pwstrDeviceId);
+		HRESULT STDMETHODCALLTYPE OnDeviceStateChanged(LPCWSTR pwstrDeviceId, DWORD dwNewState);
+		HRESULT STDMETHODCALLTYPE OnPropertyValueChanged(LPCWSTR pwstrDeviceId, const PROPERTYKEY key);
+		HRESULT STDMETHODCALLTYPE OnNotify(PAUDIO_VOLUME_NOTIFICATION_DATA pNotify);
 
-private:
-    LONG referenceCount_;
-    LPCWSTR pDeviceId_;
-};
+	private:
+		LONG referenceCount_;
+	};
 }
 #endif
